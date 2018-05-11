28f7692665f9cab3f7eeb82856c54390023d2f27
xjukebox <sebastian_09@hotmail.de>
2018-05-11 23:52:49 +0200

    PHP

--- QuizAuswertung.php
+++ QuizAuswertung.php
@@ -1,34 +1,61 @@
 <?php
+
 $Nutzer_ID = $_GET['Nutzer_ID'];
 $Name = $_GET['Name'];
 $Zeitstempel = time();
 $play= "false";	
 
+$anwendung  = $_GET["anwendung"];
 
-$array = FragenEinlesen();
-
-
-FragenSenden($array);
-if(isset($Nutzer_ID))
-{
-save($Nutzer_ID,$Name,$Zeitstempel,$play);	
+if($anwendung == "F"){
+	$array = FragenEinlesen();
+	FragenSenden($array);
 }
-function save($Nutzer_ID,$Name,$Zeitstempel,$play)
-{
-	
+
+if($anwendung == "S"){
+	PlayerOnline();
+}
+
+if(isset($Nutzer_ID)){
+	save($Nutzer_ID,$Name,$Zeitstempel,$play);	
+}
+
+function save($Nutzer_ID,$Name,$Zeitstempel,$play){
 	$saveUserOnlineRow = $Nutzer_ID.';'.$Name.';'.$Zeitstempel.';'.$play."\r\n";	
 	$saveUserOnline = fopen("Spieler.csv", "a");																							//In Anlehnung an Folien-HTML-und-PHP-komplett Von Prof.Dr. Brell S.15
 	fwrite($saveUserOnline, $saveUserOnlineRow);
 	fclose($saveUserOnline); 
 }
 
-function FragenSenden($array){
-	if(file_exists("Fragen.csv")){// prueft ob datei da ist
+function PlayerOnline(){																															//https://www.php-kurs.com/arrays-mehrdimensional.htm
+	if(file_exists("Spieler.csv")){// prueft ob datei da ist
 		$zeile = 0; 
 		$array = array();
-		$lesen = fopen("Fragen.csv", "r");																								//fgetcsv: Liest eine Zeile von der Position des Dateizeigers und prüft diese auf Semikolon-Separierte-Werte (CSV)
+		$lesen = fopen("Spieler.csv", "r");																								//fgetcsv: Liest eine Zeile von der Position des Dateizeigers und prüft diese auf Semikolon-Separierte-Werte (CSV)
+		while(($csvLesen = fgetcsv($lesen, 1000, ";")) !== FALSE){ 																				//Datei die gelesen wird(Standortdaten.csv), max. Zeichen (1000), Trennzeichen (;)
+			$array[$zeile] = $csvLesen; 																										//Doppel Array, [Zeile][0=Nutzer_ID, 1=Name, 2=Zeit, 3=Laengengrad, 4=Breitengrad, 5=Aktualisierungsintervall] NUR NR.
+			$zeile++;
+			}
+			$i=0;
+			$LaengeArray = count($array); 
+			while($i<$array){
+				$Zeitstempel2 = time();
+			$Differenz = $Zeitstempel2 - $array[$i][2]; 
+					if($Differenz < 5){
+					if($array[$i][3]=="false"){
+						echo $array[$i][1];
+						}
+			}$i++;}																											
+	}
+}
+
+function FragenSenden($array){
+	if(file_exists("Fragen.csv")){
+		$zeile = 0; 
+		$array = array();
+		$lesen = fopen("Fragen.csv", "r");																								
 		while(($csvLesen = fgetcsv($lesen, 1000, ";")) !== FALSE){ 	
-																			//Datei die gelesen wird(Standortdaten.csv), max. Zeichen (1000), Trennzeichen (;)
+																			
 		$LaengeArray = count($csvLesen); 
 		$i=0;
 		while($i<$LaengeArray){ 
