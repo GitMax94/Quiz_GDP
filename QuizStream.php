305d57ce22fd2a04e0ce5bea4bbae5836e02d1ee
AlecH95 <alec95@gmx.de>
2018-05-17 20:29:30 +0200

    html

--- QuizStream.php
+++ QuizStream.php
@@ -47,12 +47,12 @@
 }
     </style>
 
-    <div class="spieler"><h1>Quiz Spiel Stream P1 vs P2 <h1></div>
+
 <head> <title>Watch</title>
 
   <p align="center">Punkte Spieler 1  Punkte Spieler 2</p>
 <?php
- echo test;
+
 if(file_exists("quizID.csv")){// prueft ob datei da ist
  $zeile = 0;
  $array1 = array();
@@ -62,7 +62,17 @@ if(file_exists("quizID.csv")){// prueft ob datei da ist
    $zeile++;
    }
  $quizID=$array1[$zeile-1][0];
-echo "<h1>Es wurde Folgendes Quiz ausgewählt:<h1>" $quizID;}
+
+echo "<h1>Es wurde Folgendes Quiz ausgewählt:".$quizID."<h1>";}
+
+ $zeile = 0;
+ $array1 = array();
+ $lesen = fopen("SpielerListe.csv", "r");																								//fgetcsv: Liest eine Zeile von der Position des Dateizeigers und prüft diese auf Semikolon-Separierte-Werte (CSV)
+ while(($csvLesen = fgetcsv($lesen, 1000, ";")) !== FALSE){ 																				//Datei die gelesen wird(Standortdaten.csv), max. Zeichen (1000), Trennzeichen (;)
+   $array1[$zeile] = $csvLesen; 																										//Doppel Array, [Zeile][0=Nutzer_ID, 1=Name, 2=Zeit, 3=Laengengrad, 4=Breitengrad, 5=Aktualisierungsintervall] NUR NR.
+   $zeile++;
+   }
+  echo"  <div class="spieler"><h1>Quiz Spiel Stream ".$array1[0][1]." vs".$array1[1][0]."<h1></div>"
 
 ?>
 <br> </br>
