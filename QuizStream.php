816fc9064cb9bad6df28cdf8773d02d81b4b4634
AlecH95 <alec95@gmx.de>
2018-05-17 21:00:15 +0200

    update

--- QuizStream.php
+++ QuizStream.php
@@ -74,10 +74,23 @@ echo "<h1>Es wurde Folgendes Quiz ausgewählt:".$quizID."<h1>";}
    }
   echo"  <div class=spieler><h1>Quiz Spiel Stream ".$array2[$zeile2-2][1]." vs ".$array2[$zeile2-1][1]."<h1></div>";
 
-?>
-<br> </br>
-</head>
-<body >
+  <colgroup>
+  <!-- tabelen Weite -->
+<col width="37%">
+<col width="21%">
+<col width="21%">
+<col width="21%">
+</colgroup>
+
+$zeile3 = 0;
+ $array3 = array();
+ $lesen3 = fopen("Fragen.csv", "r");																								//fgetcsv: Liest eine Zeile von der Position des Dateizeigers und prüft diese auf Semikolon-Separierte-Werte (CSV)
+ while(($csvLesen2 = fgetcsv($lesen3, 1000, ";")) !== FALSE){ 																				//Datei die gelesen wird(Standortdaten.csv), max. Zeichen (1000), Trennzeichen (;)
+   $array3[$zeile3] = $csvLesen3; 																										//Doppel Array, [Zeile][0=Nutzer_ID, 1=Name, 2=Zeit, 3=Laengengrad, 4=Breitengrad, 5=Aktualisierungsintervall] NUR NR.
+   $zeile3++;
+   }
+
+echo "
 <colgroup>
   <!-- tabelen Weite -->
 <col width="37%">
@@ -92,7 +105,27 @@ echo "<h1>Es wurde Folgendes Quiz ausgewählt:".$quizID."<h1>";}
 <tr class="normal"> <td>Wie geht es dir ?</td> <td class="falsch">gut</td> <td class="richtig">schlecht</td> <td>schlecht</td>  </tr>
 <tr class="normal"> <td>Frage</td> <td>AntwortSpieler1</td> <td>AntwortSpieler2</td> <td>Richtige Antwort</td> </tr>
 <tr class="normal"> <td>Frage</td> <td>AntwortSpieler1</td> <td>AntwortSpieler2</td> <td>Richtige Antwort</td> </tr>
-</table>
+</table>"
+?>
+<br> </br>
+</head>
+<body >
+
+<colgroup>
+  <!--  
+<col width="37%">
+<col width="21%">
+<col width="21%">
+<col width="21%">
+</colgroup>
+
+<div class="tabellehintergrund">
+<table border=1 width= "100%" align="center">
+ <tr class="normal"> <th>Frage</th> <th>Spieler1 Antwort</th> <th>Spieler2 Antwort</th> <th>Richtig Antwort</th> </tr>
+<tr class="normal"> <td>Wie geht es dir ?</td> <td class="falsch">gut</td> <td class="richtig">schlecht</td> <td>schlecht</td>  </tr>
+<tr class="normal"> <td>Frage</td> <td>AntwortSpieler1</td> <td>AntwortSpieler2</td> <td>Richtige Antwort</td> </tr>
+<tr class="normal"> <td>Frage</td> <td>AntwortSpieler1</td> <td>AntwortSpieler2</td> <td>Richtige Antwort</td> </tr>
+</table> -->
 
 </div>
 
