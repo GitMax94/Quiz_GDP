c440244dbad7e85c44e875c6fa86db6c5d5625d9
xjukebox <sebastian_09@hotmail.de>
2018-05-20 00:22:09 +0200

    PHP

--- QuizStream.php
+++ QuizStream.php
@@ -1,5 +1,5 @@
 <html> <meta charset="utf-8">
-	<style>
+	<style>																		<!-- Legt das Design fest (CSS) -->														
 		table{
 			width:60%;
 		}
@@ -8,7 +8,6 @@
 			overflow:hidden;
 			vertical-align:top;
 		}
-		/* body{background-color: lightblue} */
 		.spieler{
 			background-color: Blue; color: white;text-align: center;
 		}
@@ -34,9 +33,9 @@
 			text-align: center;
 		}
     </style>
-	<head> <title>Watch</title>
+	<head> <title>Quiz Stream</title>
 		<?php
-			if(file_exists("quizID.csv")){
+			if(file_exists("quizID.csv")){									 
 				$zeile = 0;
 				$array1 = array();
 				$lesen = fopen("quizID.csv", "r");																								
@@ -44,7 +43,7 @@
 					$array1[$zeile] = $csvLesen; 																										
 					$zeile++;
 				}
-				$quizID=$array1[$zeile-1][0];
+				$quizID = $array1[$zeile-1][0];
 				$zeile3 = 0;
 				$array3 = array();
 				$lesen3 = fopen("Fragen.csv", "r");																								
@@ -53,13 +52,7 @@
 					$zeile3++;
 				}
 			}
-			$zeile2 = 0;
-			$array2 = array();
-			$lesen2 = fopen("SpielerListe.csv", "r");																								
-			while(($csvLesen2 = fgetcsv($lesen2, 0, ";")) !== FALSE){ 																				
-				$array2[$zeile2] = $csvLesen2; 																										
-				$zeile2++;
-			}
+
 			echo"  <div class=spieler><h1>Spieler1 vs Spieler2 <h1></div>";
 			echo "<div class=tabellehintergrund>";
 			echo "<table border=1 width= 100% align=center>";
@@ -80,12 +73,12 @@
 				$zeile5++;
 			}
 
-			if($array4[$zeile4-1][2]==""){
+			if($array4[$zeile4-1][2] == " "){
 				$punkte1="0";
 			}else{
 				$punkte1=$array4[$zeile4-1][2];
-			}
-			if($array5[$zeile5-1][2]==""){
+			} 
+			if($array5[$zeile5-1][2] == " "){
 				$punkte2="0";
 			}else{
 				$punkte2=$array5[$zeile5-1][2];
@@ -95,43 +88,46 @@
 			
 			$i=1;
 			$z=0; $ii=1;
-			echo "<tr class=normal> <th>Frage</th> <th>Spieler1 Antwort</th> <th>Spieler2 Antwort</th> <th>Richtig Antwort</th> </tr>";
+			echo "<tr class=normal> <th>Frage</th> <th>Spieler1 Antwort</th> 
+			<th>Spieler2 Antwort</th> <th>Richtig Antwort</th> </tr>";
 			while($i<=264){
 				$richtig = $array3[$quizID-1][$i+1];
 				$rAntwort = $richtig+1+$i;
 				$s1Antwort = $array4[$z][0]+1+$i;
 				$s2Antwort = $array5[$z][0]+1+$i;
-				if($zeile4>=$ii){
-					if($s1Antwort==$rAntwort){
-						$antwort3=$array3[$quizID-1][$s1Antwort];
+				if($zeile4 >= $ii){
+					if($s1Antwort == $rAntwort){
+						$antwort3 = $array3[$quizID-1][$s1Antwort];
 						$s1 = "richtig";
 					}else{
-						$s1 = "falsch";  $antwort3=$array3[$quizID-1][$s1Antwort];
+						$s1 = "falsch";  
+						$antwort3=$array3[$quizID-1][$s1Antwort];
 					}
 				}else{  
-					$s1 = "normal"; $antwort3="Warte auf Antwort";
+					$s1 = "normal"; 
+					$antwort3 ="Warte auf Antwort";
 				}
-				if($zeile5>=$ii){
-					if($s2Antwort==$rAntwort){
+				if($zeile5 >= $ii){
+					if($s2Antwort == $rAntwort){
 						$s2 = "richtig";  $antwort4=$array3[$quizID-1][$s2Antwort];
 					}else{
 						$s2 = "falsch";  
 						$antwort4=$array3[$quizID-1][$s2Antwort];
 					}
 				}else{
-					$s2="normal";
-					$antwort4="Warte auf Antwort";
+					$s2 = "normal";
+					$antwort4 = "Warte auf Antwort";
 				}
-				echo "<tr> <th align=\"left\">".$array3[$quizID-1][$i]."</th> <th class = ".$s1.">".$antwort3."</th> <th class =  ".$s2.">".$antwort4."</th> <th>".$array3[$quizID-1][$rAntwort]."</th> </tr>";
-				$i= $i+6;
-				$z++;$ii++;
+				echo "<tr> <th align=\"left\">".$array3[$quizID-1][$i].
+				"</th> <th class = ".$s1.">".$antwort3."</th> <th class =  ".$s2.
+				">".$antwort4."</th> <th>".$array3[$quizID-1][$rAntwort]."</th> </tr>";
+				$i = $i+6;
+				$z++;
+				$ii++;
 			}
-
 			echo "</table>";
 			echo "</div>";
-
 		?>
-	<br> </br>
 	</head>
 	<body >
 		<script language="JavaScript">
